4dcc4bb020063284978c1c6368ab22c8983fa342
xanderdice <156857212+xanderdice@users.noreply.github.com>
2024-08-05 19:15:17 -0300

    Add files via upload

--- scripts/Character.js
+++ scripts/Character.js
@@ -45,6 +45,32 @@ Character.attributes.add('playerOptions',
         ]
     });
 
+Character.attributes.add('ccd',
+    {
+        title: "ccd",
+        type: 'json',
+        schema: [
+            {
+                name: "motionThreshold",
+                type: "number",
+                default: 1,
+                title: "Motion Threshold",
+                description: "Number of meters moved in one frame before CCD is enabled"
+            }, {
+                name: "sweptSphereRadius",
+                type: "number",
+                default: .2,
+                title: "Swept Sphere Radius",
+                description: "This should be below the half extent of the collision volume. E.g For an object of dimensions 1 meter, try 0.2"
+            }, {
+                name: "contactProcessingThreshold",
+                type: "number",
+                default: 0,
+                title: "Contact Processing Threshold",
+                description: 'The constraint solver can discard solving contacts, if the distance is above this threshold. 0 by default. \n Note that using contacts with positive distance can improve stability. It increases, however, the chance of colliding with degerate contacts, such as "interior" triangle edges'
+            }
+        ]
+    });
 
 
 Character.attributes.add('sensorOptions',
@@ -426,6 +452,11 @@ Character.prototype.initialize = function () {
             angularFactor: new pc.Vec3(0, 0, 0)
         });
     }
+
+    var ccd;
+    (ccd = this.entity.rigidbody.body).setCcdMotionThreshold(this.ccd.motionThreshold);
+    ccd.setCcdSweptSphereRadius(this.ccd.sweptSphereRadius);
+    ccd.setContactProcessingThreshold(this.ccd.contactProcessingThreshold);
 
     this.prepareAnimComponent();
 
