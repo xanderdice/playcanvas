90687365859b2b0777353561960b2cce51fdc1bc
Xander Dice <xander.dice@gmail.com>
2025-09-17 21:42:12 -0300

    Se sube el juego en playcanvas a ver si lo puedo correr en github

---
+++ game/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2280132.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 214210052, 214210085, 215232956, 215310351, 240533367, 240536287, 244848186, 245038687, 245368567, 246937791, 248113238, 248120253, 248178543 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/214210045/1/basis.wasm.js', 'wasmUrl' : 'files/assets/214210043/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/214210044/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/214210048/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/214210049/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/214210047/1/ammo.js', 'preload' : true},
+];
